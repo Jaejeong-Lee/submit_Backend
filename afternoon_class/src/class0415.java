b169dbde2cb0994f80d6c12a00e115667708d12d
Jaejeong-Lee <leejae938@gmail.com>
2025-04-15 14:41:50 +0900

    first commit

---
+++ afternoon_class/src/class0415.java
@@ -0,0 +1,7 @@
+public class class0415 {
+    public static void main(String[] args) {
+        // stack /    heap     / static
+        // 주소   / 참조타입 저장 /
+        //       /             /
+     }
+}
